376f490911d8479739a8166fc5a8a56b64e81114
kiwon kwon <funnykyeon@naver.com>
2022-09-03 19:59:46 +0900

    test swagger

---
+++ src/main/java/test/testswagger/UserContact.java
@@ -0,0 +1,12 @@
+package test.testswagger;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserContact {
+    private String id;
+    private String name;
+    private String phoneNumber;
+}
